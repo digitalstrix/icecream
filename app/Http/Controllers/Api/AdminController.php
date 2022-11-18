4e12622a753b6eda89e16a94e38a294dc4807795
imkaadarsh <aadarshkavita@gmail.com>
2022-11-18 10:35:30 +0530

    chdb

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Newe;
+use App\Models\Newscategorie;
+use App\Models\Post;
 use App\Models\User;
 use App\Notifications\Authentication;
 use Dirape\Token\Token;
@@ -131,4 +134,245 @@ class AdminController extends Controller
             }
         }
     }
+
+    public function updateUser(Request $request)
+        {
+        $rules =array(
+            "token" => "required",
+            "email" => "required|email",
+        );
+        $validator= Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return $validator->errors();
+        } else{
+            if(!User::where('user_token',$request->token)->first()){
+                return response(["status" =>"failed", "message"=>"User token is not Admin Access token"], 401);
+            }
+            if(!User::where('email',$request->email)->where('user_token',$request->token)->first())
+            {
+                return response(["status" =>"failed", "message"=>"User is not Registered"], 401);
+            }
+            $user = User::where('email',$request->email)->first();
+            }
+            
+            if (isset($request->name)) {
+                $user->name = $request->name;
+            }
+            if (isset($request->mobile)) {
+                $user->mobile = $request->mobile;
+            }
+            if (isset($request->user_type)) {
+                $user->user_type = $request->user_type;
+            }
+            if ($request->hasFile('user_profile')) {
+                $file = $request->file('user_profile')->store('public/img/user_profile');
+                $user->user_profile  = $file;
+            }
+            $result= $user->save();
+            if($result){
+                $response = [
+                'status' => true,
+                "message" => "User changed successfully",
+                "updated-User" => $user
+             ];
+                return response($response, 200);
+            }
+        }
+    public function updateUserAdmin(Request $request)
+        {
+        $rules =array(
+            "token" => "required",
+            "email" => "required|email",
+        );
+        $validator= Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return $validator->errors();
+        } else{
+            if(!User::where('user_token',$request->token)->where('user_type','admin')->first()){
+                return response(["status" =>"failed", "message"=>"User token is not Admin Access token"], 401);
+            }
+            if(!User::where('email',$request->email)->where('user_token',$request->token)->first())
+            {
+                return response(["status" =>"failed", "message"=>"User is not Registered"], 401);
+            }
+            $user = User::where('email',$request->email)->first();
+            }
+            
+            if (isset($request->name)) {
+                $user->name = $request->name;
+            }
+            if (isset($request->mobile)) {
+                $user->mobile = $request->mobile;
+            }
+            if (isset($request->user_type)) {
+                $user->user_type = $request->user_type;
+            }
+            if ($request->hasFile('user_profile')) {
+                $file = $request->file('user_profile')->store('public/img/user_profile');
+                $user->user_profile  = $file;
+            }
+            $result= $user->save();
+            if($result){
+                $response = [
+                'status' => true,
+                "message" => "User changed successfully",
+                "updated-User" => $user
+             ];
+                return response($response, 200);
+            }
+        }
+        public function addPost(Request $request)
+        {
+        $rules =array(
+            "token" => "required",
+            "name" => "required",
+            "image" => "required",
+            "description" => "required",
+            "location" => "required",
+            "start" => "required",
+            "end" => "required",
+        );
+        $validator= Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return $validator->errors();
+        } else{
+            
+            if(!User::where('user_token',$request->token)->first())
+            {
+                return response(["status" =>"failed", "message"=>"Invaild User Self Token"], 401);
+            }
+            $user = User::where('user_token',$request->token)->first();
+            }
+            $blog = new Post();
+            $blog->name = $request->name;
+            $blog->description = $request->description;
+            $blog->user_id = $user->id;
+            if($request->hasFile('image'))
+            $file = $request->file('image')->store('public/img/blogs');
+            $blog->image = $file;
+            $blog->location = $request->location;
+            $blog->end = $request->end;
+            $blog->start = $request->start;
+            $result= $blog->save();
+            if($result){
+                $response = [
+                'status' => true,
+                "message" => "Post successfully Saved",
+                "updated-User" => $blog
+             ];
+                return response($response, 200);
+            }
+        }
+        public function addNewsCategory(Request $request)
+        {
+            $rules =array(
+                "category_name" => "required",
+                "token" => "required",
+             );
+            $validator= Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return $validator->errors();
+            } else {
+                if(!User::where('user_token',$request->token)->where('user_type','admin')->first()){
+                    return response(["status" => "error", "message" =>"Categorie is not created | False Token"], 401);
+                }
+                $user = new Newscategorie();
+                $user->name = $request->category_name;
+                $result = $user->save();
+                if ($result) {
+                    $response = [
+                                 'Status' => 'success',
+                                 'message' => 'Categorie was successfully created',
+                                 'data' => $user,
+                 ];
+                    return response($response, 201);
+                } else {
+                    return response(["status" => "error", "message" =>"Categorie is not created"], 401);
+                }
+            }
+        }
+        public function getNewsCategory(Request $request)
+        {
+            $rules =array(
+               
+             );
+            $validator= Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return $validator->errors();
+            } else {
+               
+                $user = Newscategorie::all();
+                if (true) {
+                    $response = [
+                                 'Status' => 'success',
+                                 'message' => 'All News Categories',
+                                 'data' => $user,
+                 ];
+                    return response($response, 201);
+                } else {
+                    return response(["status" => "error", "message" =>"Categorie is not created"], 401);
+                }
+            }
+        }
+        public function getNews(Request $request)
+        {
+            $rules =array(
+               
+             );
+            $validator= Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return $validator->errors();
+            } else {
+               
+                $user = Newscategorie::all();
+                if (true) {
+                    $response = [
+                                 'Status' => 'success',
+                                 'message' => 'All News Categories',
+                                 'data' => $user,
+                 ];
+                    return response($response, 201);
+                } else {
+                    return response(["status" => "error", "message" =>"Categorie is not created"], 401);
+                }
+            }
+        }
+        public function addNews(Request $request)
+        {
+            $rules =array(
+                "category_id" => "required",
+                "token" => "required",
+                "image" => "required",
+                "content" => "required",
+                "title" => "required",
+             );
+            $validator= Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return $validator->errors();
+            } else {
+                if(!User::where('user_token',$request->token)->where('user_type','admin')->first()){
+                    return response(["status" => "error", "message" =>"Categorie is not created | False Token"], 401);
+                }
+                $user = new Newe();
+                $user->title = $request->title;
+                $user->content = $request->content;
+                $user->category_id = $request->category_id;
+                if($request->hasFile('image'))
+                $file = $request->file('image')->store('public/img/news');
+                $user->image = $file;
+                $result = $user->save();
+                if ($result) {
+                    $response = [
+                                 'Status' => 'success',
+                                 'message' => 'Categorie was successfully created',
+                                 'data' => $user,
+                 ];
+                    return response($response, 201);
+                } else {
+                    return response(["status" => "error", "message" =>"Categorie is not created"], 401);
+                }
+            }
+        }
+        
+        
 }
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,12 @@ Route::post("/register",'App\Http\Controllers\Api\AdminController@register');
 Route::get("/getSite",'App\Http\Controllers\SiteController@getSite');
 Route::post("/otplogin",'App\Http\Controllers\Api\AdminController@otplogin');
 Route::post("/otpValidate",'App\Http\Controllers\Api\AdminController@otpValidate');
+Route::post("/updateUser",'App\Http\Controllers\Api\AdminController@updateUser');
+Route::post("/updateUserAdmin",'App\Http\Controllers\Api\AdminController@updateUserAdmin');
+Route::post("/addPost",'App\Http\Controllers\Api\AdminController@addPost');
+Route::post("/addNewsCategory",'App\Http\Controllers\Api\AdminController@addNewsCategory');
+Route::get("/getNewsCategory",'App\Http\Controllers\Api\AdminController@getNewsCategory');
+Route::post("/addNews",'App\Http\Controllers\Api\AdminController@addNews');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
